2e1e05e4588ad67080109c295f408e9f7850dc1d
shivabalaji764 <shivatalla764@gmail.com>
2026-03-02 17:10:49 +0530

    Empty spring project

---
+++ booking/src/test/java/com/sss/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sss.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
